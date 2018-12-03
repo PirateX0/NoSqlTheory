2497bd9ad2277915eaa3727659c9305aeb40eeaa
PirateX0 <295528322@qq.com>
2018-12-03 23:20:34 +0800

    study code.

---
+++ DotNetMemoryCache/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetMemoryCache
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MemoryCache.Default.Add("age", 18, DateTimeOffset.Now.AddMinutes(1));
+            MemoryCache.Default.Add("name", "dalong", DateTimeOffset.Now.AddMinutes(1));
+            MessageBox.Show("set successfully!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int age = 0;
+            string name = null;
+            if (MemoryCache.Default.Contains("name"))
+            {
+                 age = (int)MemoryCache.Default["age"];
+                name = (string)MemoryCache.Default["name"];
+            }
+            MessageBox.Show(name+":"+ age.ToString());
+        }
+    }
+}
---
+++ MemcachedTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enyim.Caching;
+using Enyim.Caching.Configuration;
+using Enyim.Caching.Memcached;
+
+namespace MemcachedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemcachedClientConfiguration mcConfig = new MemcachedClientConfiguration();
+            mcConfig.AddServer("127.0.0.1:11211");//必须指定端口
+            using (MemcachedClient client = new MemcachedClient(mcConfig))
+            {
+                //client.Store(Enyim.Caching.Memcached.StoreMode.Set, "name", "yzk");
+                //string name=(string) client.Get("name");
+
+                //client.Store(Enyim.Caching.Memcached.StoreMode.Set, "p1", new Person { Name="dalong",Age=18});
+
+                Person person=(Person) client.Get("p1");
+
+                Console.WriteLine(person.Name+","+person.Age);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
